27f2d4baf37e2c0f966ac8c72c628d5fbbfed4e3
GrigorSmi <grigor.smi.smirnov@yandex.ru>
2026-04-19 12:37:00 +0300

    Исправлено согласно комментариям1

--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.gym;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import java.time.DayOfWeek;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
